16869cfc7374eff9d5bac201cf7d33cd4bed5059
Yuri Canuto <yuri.canuto@smplaces.com.br>
2022-09-07 04:05:12 -0300

    feat(Sidebar): Finalizando a Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1497366811353-6870744d04b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=50" alt="" />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/7301783?v=4" alt="" />
+
+        <strong>Yuri Canuto</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
